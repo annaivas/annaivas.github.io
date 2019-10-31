c98b93397e8289a81b27745c843175b82e3b606c
annaivas <anna.iva617@gmail.com>
2019-10-31 21:25:27 +0300

    working on site dis

--- main.js
+++ main.js
@@ -5,3 +5,79 @@ window.addEventListener("scroll", function ()
     let offset = window.pageYOffset;
     parallax.style.backgroundPositionY = offset * 0.7 + "px";
 })
+
+class TypeWriter 
+{
+    constructor(txtElement, words, wait = 3000) {
+        this.txtElement = txtElement;
+        this.words = words;
+        this.txt = '';
+        this.wordIndex = 0;
+        this.wait = parseInt(wait, 10);
+        this.type();
+        this.isDeleting = false;
+    }
+
+    type() {
+        const current = this.wordIndex % this.words.length;
+        const fullTxt = this.words[current];
+
+        if(this.isDeleting)
+        {
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
+        }
+        else
+        {
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+        let typeSpeed = 300;
+
+        if(this.isDeleting)
+        {
+            typeSpeed /= 2;
+        }
+
+        if(!this.isDeleting && this.txt === fullTxt)
+        {
+            typeSpeed = this.wait;
+            this.isDeleting = true;
+        }
+        else if(this.isDeleting && this.txt === '')
+        {
+            this.isDeleting = false;
+            this.wordIndex++;
+            typeSpeed = 500;
+        }
+
+        setTimeout(() => this.type(), typeSpeed)
+    }
+}
+
+
+
+document.addEventListener('DOMContentLoaded', init);
+
+function init()
+{
+    const txtElement = document.querySelector('.txt-type');
+    const words = JSON.parse(txtElement.getAttribute('data-words'));
+    const wait = txtElement.getAttribute('data-wait');
+
+    new TypeWriter(txtElement, words, wait);
+}
+
+ 
+/*<script>
+$(document).ready(function(){
+    PopUpHide();
+});
+function PopUpShow(){
+    $("#popup1").show();
+}
+function PopUpHide(){
+    $("#popup1").hide();
+}
+</script>*/
